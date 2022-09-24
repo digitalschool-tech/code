7fdb8033dc9431f383dbced8ae2238118535997b
Denis Hoti <denis.hoti@starlabspro.com>
2022-09-24 20:35:16 +0200

    Project Setup

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: [
+    './resources/views/**/*.blade.php',
+    './resources/css/**/*.css',
+  ],
+  content: [],
+  theme: {
+    colors: {
+    },
+    textColors: {
+    },
+    extend: {},
+  },
+  plugins: [],
+}
